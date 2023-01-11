2432ba9f38316f39e377e2e8f56d75129f1e6566
ksvistunova <uhgdy_1@mail.ru>
2023-01-11 18:03:00 +0300

    Базовая страница для проекта: BasePage

--- conftest.py
+++ conftest.py
@@ -10,9 +10,9 @@ def pytesrt_addoption(parser):
 
 @pytest.fixture(scope="function")
 def browser(request):
-    user_language = request.config.getoption("language")
+    #user_language = request.config.getoption("language")
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    #options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     browser = webdriver.Chrome(options=options)
     yield browser
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -7,7 +7,4 @@ def test_button(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     time.sleep(3)
 
-    #поиск кнопки по классу
-    button = browser.find_elements(By.CSS_SELECTOR, "button[class='btn btn-lg btn-primary btn-add-to-basket']")
-
-    assert len(button)==1, "Кнопка не найдена"
+    assert browser.find_element(By.XPATH, '//button[@class="btn btn-lg btn-primary btn-add-to-basket"]')
